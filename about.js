7e85a5f5df695296d40e81264dacbc2c958b6000
Azhan-Aathir <47764425+Azhan-Aathir@users.noreply.github.com>
2021-05-18 14:22:19 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,56 @@
+u3$(document).ready(function(){
+  $(window).resize(function(){
+  	var width = window.innerWidth;
+	  if (width < 751) {
+	    document.getElementById("abtimg").src="about/abt3.png";  
+      }
+      else
+          {
+           document.getElementById("abtimg").src="about/abt2.png";
+          }
+        
+  })
+})
+
+$(document).ready(function(){
+  //$(window).onload(function(){
+  	var width = window.innerWidth;
+	  if (width < 751) {
+	    document.getElementById("abtimg").src="about/abt3.png";  
+      }
+      else
+          {
+           document.getElementById("abtimg").src="about/abt2.png";
+          }
+        
+  })
+//})
+
+
+$(document).ready(function(){
+  $(window).resize(function(){
+  	var width = window.innerWidth;
+	  if (width < 751) {
+	    document.getElementById("abtimg2").src="trainer/tp2.png";  
+      }
+      else
+          {
+           document.getElementById("abtimg2").src="trainer/tp3.jpg";
+          }
+        
+  })
+})
+
+$(document).ready(function(){
+  //$(window).onload(function(){
+  	var width = window.innerWidth;
+	  if (width < 751) {
+	    document.getElementById("abtimg2").src="trainer/tp2.png";  
+      }
+      else
+          {
+           document.getElementById("abtimg2").src="trainer/tp3.jpg";
+          }
+        
+  })
+//})
